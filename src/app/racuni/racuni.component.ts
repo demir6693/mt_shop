c9f78bb18ad33f54ca4868a36ed229c18e289b97
demir6693 <demiretf@gmail.com>
2018-12-10 23:42:44 +0100

    recipe finish

--- src/app/racuni/racuni.component.ts
+++ src/app/racuni/racuni.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,15 +8,47 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RacuniComponent implements OnInit {
 
+  getRacuniData: any = [];
+  getDnevniIznos: number;
+  getDnevniZarada: number;
+  getMesecniIznos: number;
+  getMesecniZarada: number;
+  getGodisnjiIznos: number;
+  getGodisnjiZarada: number;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
 
-    this.http.get('http://localhost:3000/getRacuni/65')
+    this.getDnevniIznos = 0;
+    this.getDnevniZarada = 0;
+    this.http.get('http://localhost:3000/racuni/getdnevni')
     .subscribe(
       (data) => {
-        console.log(data);
+        this.getRacuniData = data;
+
+        for(var i = 0; i < this.getRacuniData.length; i++)
+        {
+          this.getDnevniIznos += parseInt(this.getRacuniData[i].iznos);
+          this.getDnevniZarada += parseInt(this.getRacuniData[i].iznos_nabavna);
+          this.getDnevniZarada = this.getDnevniIznos - this.getDnevniZarada;
+        } 
       }
     );
 
+    this.http.get('http://localhost:3000/racuni/getmesecni')
+    .subscribe(
+      (data) => {
+        this.getMesecniZarada = parseInt(data[0]['(SUM(iznos)- SUM(iznos_nabavna))']);
+        this.getMesecniIznos = parseInt(data[0]['SUM(iznos)']);
+      }
+    );
+
+    this.http.get('http://localhost:3000/racuni/getgodisnji')
+    .subscribe(
+      (data) => {
+        this.getGodisnjiZarada = parseInt(data[0]['(SUM(iznos)- SUM(iznos_nabavna))']);
+        this.getGodisnjiIznos = parseInt(data[0]['SUM(iznos)']);
+      }
+    );
+    }
 }
